ccb3ad46910d2ca3f2c526ad2dea620e0a8e4761
Jayasurya Midde <jayanthjay751@gmail.com>
2026-04-23 07:17:11 +0530

    fix: Update VercelBadge SVG dimensions and path for improved rendering

--- src/components/sg/VercelBadge.tsx
+++ src/components/sg/VercelBadge.tsx
@@ -12,12 +12,11 @@ export function VercelBadge() {
       <svg
         width="16"
         height="16"
-        viewBox="0 0 283 64"
-        className="h-4 w-auto"
+        viewBox="0 0 116 100"
+        className="h-3 w-auto"
         fill="white"
       >
-        <path d="M141.04 16c-11.04 0-19 7.2-19 18s8.96 18 20 18c6.67 0 12.55-2.64 16.19-7.09l-7.65-4.42c-2.02 2.21-5.09 3.5-8.54 3.5-4.79 0-8.86-2.5-10.37-6.5h28.02c.22-1.12.35-2.28.35-3.5 0-10.79-7.96-17.99-19-17.99zm-9.46 14.5c1.52-4.04 5.29-6.75 11.07-6.75 5.54 0 9.29 2.67 10.88 6.75h-21.95z" />
-        <path d="M185.52.63L142.3 64h-13.5L185.52.63z" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M57.5 0L115 100H0L57.5 0Z" />
       </svg>
       <span className="text-xs font-medium text-white/80">Powered by Vercel</span>
       <ExternalLink className="h-3.5 w-3.5 text-white/60" />
